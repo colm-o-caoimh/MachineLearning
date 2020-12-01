bacd69d176b6fea883cc645c815f1bc274ffa4b2
Colm <colmocaoimh@outlook.com>
2020-12-01 18:24:51 +0000

    python script for web service

---
+++ ajax/powerproduction.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+pp = pd.read_csv('powerproduction.csv')
+#print(pp.head())
+
+speed_df = pp.drop('power',axis='columns')
+#print(speed_df)
+
+power_df = pp.drop('speed',axis='columns')
+#print(power_df)
+
+reg = LinearRegression()
+reg.fit(speed_df, power_df)
+#print(reg.predict([[9.746]]))
+
+
+
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('form.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+
+	wind_speed = request.form['email']
+	# name = request.form['name']
+
+	#if name and wind_speed:
+	if float(wind_speed) > 0:
+		power = reg.predict([[float(wind_speed)]])
+
+		if power < 0:
+			return jsonify({'name' : 0})
+
+		return jsonify({'name' : float(power)})
+
+	return jsonify({'error' : 'Missing data!'})
+
+if __name__ == '__main__':
+	app.run(debug=True)
