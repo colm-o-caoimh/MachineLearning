d922bd27121a420d0d4e8999b016144c7a67917a
Colm <colmocaoimh@outlook.com>
2020-12-28 22:38:46 +0000

    made adjustments to app server to save model

--- powerproduction.py
+++ powerproduction.py
@@ -3,6 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+import tensorflow.keras as kr
+from ipynb.fs.defs.mls_project2020 import model_predict
 
 pp = pd.read_csv('powerproduction.csv')
 #print(pp.head())
@@ -16,13 +18,12 @@ power_df = pp.drop('speed',axis='columns')
 reg = LinearRegression()
 reg.fit(speed_df, power_df)
 #print(reg.predict([[9.746]]))
- 
- 
+
 
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
-
+ 
 @app.route('/')
 def index():
 	return render_template('form.html')
@@ -35,7 +36,9 @@ def process():
 
 	#if name and wind_speed:
 	if float(wind_speed) > 0:
-		power = reg.predict([[float(wind_speed)]])
+		# power = reg.predict([[float(wind_speed)]])
+		power = model_predict([[float(wind_speed)]])
+		power = power.item(0)
 
 		if power < 0:
 			return jsonify({'name' : 0})
