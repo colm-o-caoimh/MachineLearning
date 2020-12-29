01b79bda47e2ab5aa1a5f770aa05f2c95790f8b7
Colm <colmocaoimh@outlook.com>
2020-12-29 09:05:25 +0000

    save model and reload in app server - working

--- powerproduction.py
+++ powerproduction.py
@@ -4,21 +4,21 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 import tensorflow.keras as kr
-from ipynb.fs.defs.mls_project2020 import model_predict
 
-pp = pd.read_csv('powerproduction.csv')
-#print(pp.head())
+# pp = pd.read_csv('powerproduction.csv')
+# #print(pp.head())
 
-speed_df = pp.drop('power',axis='columns')
-#print(speed_df)
+# speed_df = pp.drop('power',axis='columns')
+# #print(speed_df)
 
-power_df = pp.drop('speed',axis='columns')
-#print(power_df)
+# power_df = pp.drop('speed',axis='columns')
+# #print(power_df)
 
-reg = LinearRegression()
-reg.fit(speed_df, power_df)
-#print(reg.predict([[9.746]]))
+# reg = LinearRegression()
+# reg.fit(speed_df, power_df)
+# #print(reg.predict([[9.746]]))
 
+new_model = kr.models.load_model('my_model.h5')
 
 from flask import Flask, render_template, request, jsonify
 
@@ -37,7 +37,7 @@ def process():
 	#if name and wind_speed:
 	if float(wind_speed) > 0:
 		# power = reg.predict([[float(wind_speed)]])
-		power = model_predict([[float(wind_speed)]])
+		power = new_model.predict([[float(wind_speed)]])
 		power = power.item(0)
 
 		if power < 0:
